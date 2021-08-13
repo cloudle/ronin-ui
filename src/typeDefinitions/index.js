bd9550d8b56cacb5e14e12dc766a2dc027b384c9
Cloud Le <lehaoson@gmail.com>
2021-08-13 18:23:53 +0700

    flow.js typeDefinitions and common utils (e.g colors)

---
+++ src/typeDefinitions/index.js
@@ -0,0 +1,22 @@
+export type Element = React.Element<*>;
+
+export type Style =
+	| { [key: string]: any }
+	| number
+	| false
+	| null
+	| void
+	| Array<Style>;
+
+export type Layout = {
+	x: number,
+	y: number,
+	width: number,
+	height: number,
+};
+
+export type LayoutEvent = {
+	nativeEvent: {
+		layout: Layout,
+	},
+};
---
+++ src/utils/global/constant.js
@@ -0,0 +1,8 @@
+export const colors = {
+	roninBlue: '#1273EA',
+};
+
+export const textColors = {
+	gray: '#151A30',
+	lightGray: '#57627B',
+};
