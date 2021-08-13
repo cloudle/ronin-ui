d35d6060f7935281dc187ef58ebe2d237df51d57
Cloud Le <lehaoson@gmail.com>
2021-08-14 05:25:22 +0700

    setEnvironments on webpack, required by reanimated 2

--- wings.config.js
+++ wings.config.js
@@ -29,10 +29,25 @@ const copyAssets = (configs) => {
 	return configs;
 };
 
+const setEnvironments = (configs, { webpack, wingsConfig, }) => {
+	const { DefinePlugin, } = webpack;
+	const env = wingsConfig.env();
+	const isProduction = wingsConfig.isProduction(env);
+
+	configs.plugins[0] = new DefinePlugin({
+		process: { env: {} },
+		__DEV__: !isProduction,
+		ENV: JSON.stringify(env),
+	});
+
+	return configs;
+};
+
 module.exports = {
 	webpackConfigs: [
 		splitBundle,
 		copyAssets,
+		setEnvironments,
 	],
 	moduleAlias: (isProduction) => {
 		return {
