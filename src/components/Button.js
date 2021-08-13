ded5e75d4da4cde3298fe334b04c661010b13647
Cloud Le <lehaoson@gmail.com>
2021-08-14 04:32:57 +0700

    adjust design (continued), gradient color for Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text, } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import type { Style, } from 'typeDefinitions';
 import { colors, sizes, } from 'utils/global';
 
 type Props = {
 	style?: Style,
+	contentContainerStyle?: Style,
 	captionStyle?: Style,
 	caption?: string,
 	disabled?: boolean,
+	onPress?: Function,
 };
 
 const Button = (props: Props) => {
-	const { style, captionStyle, caption, disabled, } = props;
+	const { style, contentContainerStyle, captionStyle, caption, disabled, onPress, } = props;
+	const backgroundColors = disabled ? ['#F7F9FC', '#F7F9FC'] : ['#1273EA', '#1C94F4'];
 
 	return <TouchableOpacity
-		style={[styles.container, disabled && styles.disabledContainer, style]}>
-		<Text style={[styles.caption, disabled && styles.disabledCaption, captionStyle]}>
-			{caption}
-		</Text>
+		style={style}
+		onPress={onPress}>
+		<LinearGradient
+			style={[styles.contentContainer, contentContainerStyle]}
+			colors={backgroundColors}
+			start={{ x: 0, y: 0.5 }}
+			end={{ x: 1, y: 0.5 }}>
+			<Text style={[styles.caption, disabled && styles.disabledCaption, captionStyle]}>
+				{caption}
+			</Text>
+		</LinearGradient>
 	</TouchableOpacity>;
 };
 
@@ -29,8 +40,7 @@ Button.defaultProps = {
 export default Button;
 
 const styles = StyleSheet.create({
-	container: {
-		backgroundColor: colors.roninBlue,
+	contentContainer: {
 		borderRadius: sizes.normalRadius,
 		padding: 12,
 	},
