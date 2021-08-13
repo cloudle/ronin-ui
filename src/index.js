64e3811c1ab757e9279f1c008f287c718319f57e
Cloud Le <lehaoson@gmail.com>
2021-08-14 05:33:53 +0700

    expose navigate (router) for non-component module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React, { Fragment, } from 'react';
+import React, { Fragment, useEffect, } from 'react';
 import { Provider, } from 'react-redux';
 import { SafeAreaProvider, } from 'react-native-safe-area-context';
+import { useNavigate, } from 'react-router';
 
 import Dashboard from 'routes/Dashboard';
 import Login from 'routes/Login';
 import Send from 'routes/Send';
 import { Router, } from 'utils/router';
 import { useRoutes, } from 'utils/effect';
+import { setNavigate, } from 'utils/global';
 import { store, } from 'store';
 
 type Props = {
@@ -14,6 +16,12 @@ type Props = {
 };
 
 const App = (props: Props) => {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setNavigate(navigate);
+	}, []);
+
 	const routedElement = useRoutes([{
 		path: '/login', element: <Login/>,
 	}, {
--- src/utils/global/index.js
+++ src/utils/global/index.js
@@ -1,1 +1,11 @@
 export * from './constant';
+
+const routerInstance = {
+	navigate: () => {},
+};
+
+export const setNavigate = (val) => {
+	routerInstance.navigate = val;
+};
+
+export const router = routerInstance;
